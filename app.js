352ae639c2eb2d288a3a3025f32729916ba39392
Nishkarsh <nishkarsh.gupta1036@gmail.com>
2021-07-09 00:17:29 +0530

    fina step

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ mongoose.set('useCreateIndex', true);
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    googleId: String
+    googleId: String,
+    secret: String
 });
 
 //plugin
@@ -92,17 +93,46 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
+    User.find({"secret": {$ne: null}}, function(err, foundUsers) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUsers) {
+                res.render("secrets", {usesrWithSecrets: foundUsers})
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res) {
     if(req.isAuthenticated()) {
-        res.render("secrets");
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
-})
+});
+
+app.post("/submit", function(req, res) {
+    console.log(req.user.id);
+    const submittedSecret = req.body.secret;
+    User.findById(req.user.id, function(err, foundUser) {
+        if(err) {
+            console.log(err);
+        } else {
+            if(foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
+});
 
 app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
-})
+});
 
 //register using passport
 app.post("/register", function(req, res) {
